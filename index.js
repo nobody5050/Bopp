84d8d1480fc278dd1298b7f5b9264444eb2349e2
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2021-03-10 08:50:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,105 +15,19 @@ firebase.initializeApp(firebaseConfig);
 var storage = firebase.storage();
 var storageRef = firebase.storage().ref();
 
-function Upload() {
+function Upload() {      
+  // Created a Storage Reference with root dir
+  var storageRef = firebase.storage().ref();
+  // Get the file from DOM
   var file = document.getElementById("files").files[0];
   console.log(file);
+      
   //dynamically set reference to the file name
   var thisRef = storageRef.child(file.name);
-  
-  // Create the file metadata
-  var metadata = {
-    contentType: 'image/jpeg'
-  };
 
-  // Upload file and metadata to the object 'images/mountains.jpg'
-  var uploadTask = storageRef.child('images/' + file.name).put(file, metadata);
-
-  // Listen for state changes, errors, and completion of the upload.
-  uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-                (snapshot) => {
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-      case firebase.storage.TaskState.PAUSED: // or 'paused'
-        console.log('Upload is paused');
-        break;
-      case firebase.storage.TaskState.RUNNING: // or 'running'
-        console.log('Upload is running');
-        break;
-    }
-  }, 
-                (error) => {
-    // A full list of error codes is available at
-    // https://firebase.google.com/docs/storage/web/handle-errors
-    switch (error.code) {
-      case 'storage/unauthorized':
-        // Create the file metadata
-var metadata = {
-  contentType: 'image/jpeg'
-};
-
-// Upload file and metadata to the object 'images/mountains.jpg'
-var uploadTask = storageRef.child('images/' + file.name).put(file, metadata);
-
-// Listen for state changes, errors, and completion of the upload.
-uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-  (snapshot) => {
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-      case firebase.storage.TaskState.PAUSED: // or 'paused'
-        console.log('Upload is paused');
-        break;
-      case firebase.storage.TaskState.RUNNING: // or 'running'
-        console.log('Upload is running');
-        break;
-    }
-  }, 
-  (error) => {
-    // A full list of error codes is available at
-    // https://firebase.google.com/docs/storage/web/handle-errors
-    switch (error.code) {
-      case 'storage/unauthorized':
-        // User doesn't have permission to access the object
-        break;
-      case 'storage/canceled':
-        // User canceled the upload
-        break;
-
-      // ...
-
-      case 'storage/unknown':
-        // Unknown error occurred, inspect error.serverResponse
-        break;
-    }
-  }, 
-  () => {
-    // Upload completed successfully, now we can get the download URL
-    uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-      console.log('File available at', downloadURL);
-    });
-  }
-);// User doesn't have permission to access the object
-        break;
-      case 'storage/canceled':
-        // User canceled the upload
-        break;
-
-      // ...
-
-      case 'storage/unknown':
-        // Unknown error occurred, inspect error.serverResponse
-        break;
-    }
-  }, 
-  () => {
-    // Upload completed successfully, now we can get the download URL
-    uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-      console.log('File available at', downloadURL);
-    });
-  }
-);
+  //put request upload file to firebase storage
+  thisRef.put(file).then(function(snapshot) {
+    alert("File Uploaded")
+    console.log('Uploaded a blob or file!');
+  });
 }
