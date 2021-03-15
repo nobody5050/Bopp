0562464a2b2d697a03cee167998e9ba740650ad5
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2021-03-15 00:38:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,6 @@
-// Set the configuration for your app
-// TODO: Replace with your app's config object
-var firebaseConfig = {
-  apiKey: "AIzaSyAj_ilW3sPYkKg9zd3tbZcvGLg1ou1l00o",
-  authDomain: "test-e0e57.firebaseapp.com",
-  databaseURL: "https://test-e0e57.firebaseio.com",
-  projectId: "test-e0e57",
-  storageBucket: "test-e0e57.appspot.com",
-  messagingSenderId: "734076048929",
-  appId: "1:734076048929:web:41612dabaf67e345abb560"
-};
-firebase.initializeApp(firebaseConfig);
-
-// Get a reference to the storage service, which is used to create references in your storage bucket
-var storage = firebase.storage();
-var storageRef = firebase.storage().ref();
-
-function Upload() {      
-  // Created a Storage Reference with root dir
-  var storageRef = firebase.storage().ref();
-  // Get the file from DOM
-  const file = document.getElementById("files").files[0];
-  console.log(file);
-      
-  const uuid = Math.random().toString(36).slice(2)
-  
-  //dynamically set reference to the file name
-  var thisRef = storageRef.child(uuid);
-
-  //put request upload file to firebase storage
-  thisRef.put(file).then(function(snapshot) {
-    alert("File Uploaded")
-    console.log('Uploaded a blob or file!');
-    const publicUrl = (`https://firebasestorage.googleapis.com/v0/b/test-e0e57.appspot.com/o/${file.name}?alt=media&token=c45a9272-2345-488f-b396-5ebe3f37d8b5`);
-    console.log(publicUrl);
-  });
-}
+const accountName = "bopp";
+const sasString = "?sv=2020-02-10&ss=b&srt=sco&sp=rc&se=2099-03-15T07:03:35Z&st=2021-03-14T23:03:35Z&spr=https&sig=6pmhaF%2BNSWmIoXWcaC%2BGRd60F1EWWG%2BcktZlsAykqBY%3D";
+const containerName = "images";
+const containerURL = new azblob.ContainerURL(
+    `https://${accountName}.blob.core.windows.net/${containerName}?${sasString}`,
+    azblob.StorageURL.newPipeline(new azblob.AnonymousCredential));
