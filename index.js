9500ca3021933b0e9d228a6b57fc007de485f9f3
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2021-03-15 00:41:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const selectButton = document.getElementById("submit");
+const fileInput = document.getElementById("files");
 const accountName = "bopp";
 const sasString = "?sv=2020-02-10&ss=b&srt=sco&sp=rc&se=2099-03-15T07:03:35Z&st=2021-03-14T23:03:35Z&spr=https&sig=6pmhaF%2BNSWmIoXWcaC%2BGRd60F1EWWG%2BcktZlsAykqBY%3D";
 const containerName = "images";
